4c76210c3f08eeb829580c6eb2c4282f5de38155
Romain VORLET <romain.vorlet@gmail.com>
2023-12-19 23:20:22 +0100

    refacto ResponseManagerHTTP

--- src/org/immo/model/ResponseManagerHTTP.java
+++ src/org/immo/model/ResponseManagerHTTP.java
@@ -2,205 +2,27 @@ package org.immo.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.immo.exceptions.UnknownResponseCode;
-import org.immo.geojson.adresseban.AdresseBAN;
-import org.immo.geojson.feuille.Feuille;
-import org.immo.geojson.mutation.Mutation;
-import org.immo.geojson.geomutation.Geomutation;
-import org.immo.geojson.parcelle.Parcelle;
 import org.immo.servicepublicapi.*;
 
 import java.io.IOException;
 import java.util.Optional;
 
-public class ResponseManagerHTTP {
+public class ResponseManagerHTTP<T> {
 
     /**
      * A partir du code retour de l'appel à l'API Adresse, retourne ou non un POJO représentant les adresses contenues
      * dans la réponse
-     * @param requeteAdresse
+     * @param request
      * @return Un Optional contenant null ou un POJO AdresseBAN
      * @throws IOException
      */
-    public Optional<AdresseBAN> controleAdresseRetour(AdresseAPI requeteAdresse) throws IOException, UnknownResponseCode {
-        int codeRetour = requeteAdresse.getConn().getResponseCode();
-        AdresseBAN adresseReponse;
-        switch(codeRetour) {
-            case 200:
-                System.out.println("Code retour : 200. Requête OK");
-                String jsonReponse = requeteAdresse.readReponseFromAPI(requeteAdresse.getConn());
-                if (jsonReponse.isEmpty()) {
-                    System.out.println("Le contenu de la réponse sur l'adresse est vide");
-                    return Optional.empty();
-                }
-                else {
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    adresseReponse = objectMapper.readValue(jsonReponse, AdresseBAN.class);
-                }
-                return Optional.of(adresseReponse);
-
-            case 400:
-                System.out.println("Code retour : 400. Les critères de recherche sont incorrectes");
-                System.out.println("Motif : "+requeteAdresse.getConn().getResponseMessage());
-                break;
-
-            case 403:
-                System.out.println("Code retour : 403. Vous ne pouvez pas accéder à ce contenu");
-                break;
-
-            case 404:
-                System.out.println("Code retour : 404. L'API n'existe plus");
-                break;
-            /**
-             * Limité à 50/secondes/IP
-             */
-            case 429:
-                System.out.println("Code retour : 429. Trop de requêtes");
-                break;
-
-            case 500:
-                System.out.println("Code retour : 500. Avez-vous une connexion internet ?");
-                break;
-
-            default:
-                System.out.println("Cas non prévu, il faut checker les logs");
-                throw new UnknownResponseCode("Code erreur inconnu");
-        }
-        return Optional.empty();
-    }
-
-    /**
-     * A partir d'un appel à l'API du cadastre /parcelle, vérifie le code retour et renvoie un POJO d'une parcelle.
-     * @param requeteParcelle
-     * @return un Optional contenant ou non un POJO Parcelle
-     * @throws IOException
-     * @throws UnknownResponseCode
-     */
-    public Optional<Parcelle> controleParcelleRetour(ParcelleAPI requeteParcelle) throws IOException, UnknownResponseCode {
-        int codeRetour = requeteParcelle.getConn().getResponseCode();
-        Parcelle parcelleReponse;
-        switch(codeRetour) {
-            case 200:
-                System.out.println("Code retour : 200. Requête OK");
-                String jsonReponse = requeteParcelle.readReponseFromAPI(requeteParcelle.getConn());
-                ObjectMapper objectMapper = new ObjectMapper();
-                parcelleReponse = objectMapper.readValue(jsonReponse, Parcelle.class);
-                System.out.println(parcelleReponse.showTerrainContent());
-                if (parcelleReponse.getNumberReturned() == 0) {
-                    System.out.println("Le contenu de la réponse sur la parcelle est vide");
-                    return Optional.empty();
-                }
-                else return Optional.of(parcelleReponse);
-
-            case 400:
-                System.out.println("Code retour : 400. Les critères de recherche sont incorrectes");
-                System.out.println("Motif : "+requeteParcelle.getConn().getResponseMessage());
-                break;
-
-            case 403:
-                System.out.println("Code retour : 403. Vous ne pouvez pas accéder à ce contenu");
-                break;
-
-            case 404:
-                System.out.println("Code retour : 404. L'API n'existe plus");
-                break;
-            /**
-             * Limité à 50/secondes/IP
-             */
-            case 429:
-                System.out.println("Code retour : 429. Trop de requêtes");
-                break;
-
-            case 500:
-                System.out.println("Code retour : 500. Avez-vous une connexion internet ?");
-                break;
-
-            default:
-                System.out.println("Cas non prévu, il faut checker les logs");
-                throw new UnknownResponseCode("Code erreur inconnu");
-        }
-        return Optional.empty();
-    }
-
-    /**
-     * A partir d'une coordonnées de parcelle ou de feuille, voire d'un point, récupère la liste des mutations.
-     * @param requeteGeomutation
-     * @return
-     * @throws IOException
-     * @throws UnknownResponseCode
-     */
-    public Optional<Geomutation> controleGeomutationRetour(GeomutationAPI requeteGeomutation) throws IOException, UnknownResponseCode {
-        int codeRetour = requeteGeomutation.getConn().getResponseCode();
-        Geomutation geomutationReponse;
-        switch(codeRetour) {
-            case 200:
-                System.out.println("Code retour : 200. Requête OK");
-                String jsonReponse = requeteGeomutation.readReponseFromAPI(requeteGeomutation.getConn());
-                ObjectMapper objectMapper = new ObjectMapper();
-                geomutationReponse = objectMapper.readValue(jsonReponse, Geomutation.class);
-                System.out.println(geomutationReponse.showGeomutationContent());
-                if (geomutationReponse.getCount() == 0) {
-                    System.out.println("Le contenu de la réponse sur la geomutation est vide");
-                    return Optional.empty();
-                }
-                else return Optional.of(geomutationReponse);
-
-            case 400:
-                System.out.println("Code retour : 400. Les critères de recherche sont incorrectes");
-                System.out.println("Motif : "+requeteGeomutation.getConn().getResponseMessage());
-                break;
-
-            case 403:
-                System.out.println("Code retour : 403. Vous ne pouvez pas accéder à ce contenu");
-                break;
-
-            case 404:
-                System.out.println("Code retour : 404. L'API n'existe plus");
-                break;
-            /**
-             * Limité à 50/secondes/IP
-             */
-            case 429:
-                System.out.println("Code retour : 429. Trop de requêtes");
-                break;
-
-            case 500:
-                System.out.println("Code retour : 500. Avez-vous une connexion internet ?");
-                break;
-
-            default:
-                System.out.println("Cas non prévu, il faut checker les logs");
-                throw new UnknownResponseCode("Code erreur inconnu");
-        }
-        return Optional.empty();
-    }
-
-    /**
-     * A partir d'un id de mutation, récupère l'intégralité des détails de l'opération acccessible depuis l'API DVF+
-     * @param requeteMutation
-     * @return
-     * @throws IOException
-     * @throws UnknownResponseCode
-     */
-    public Optional<Mutation> controleMutationRetour(MutationAPI requeteMutation) throws IOException, UnknownResponseCode {
-        if (isSuccess(requeteMutation)) {
-            Mutation MutationReponse;
-            String jsonReponse = requeteMutation.readReponseFromAPI(requeteMutation.getConn());
+    public Optional<T> getAPIReturn(AbstractRequestAPI request, Class<T> typeClass) throws IOException, UnknownResponseCode {
+        if (isSuccess(request)) {
+            T objectResponse;
+            String jsonResponse = request.readReponseFromAPI(request.getConn());
             ObjectMapper objectMapper = new ObjectMapper();
-            MutationReponse = objectMapper.readValue(jsonReponse, Mutation.class);
-            System.out.println(MutationReponse.showMutationContent());
-            return Optional.of(MutationReponse);
-        }
-        else return Optional.empty();
-    }
-
-    public Optional<Feuille> controleFeuilleRetour(FeuilleAPI requeteFeuille) throws IOException, UnknownResponseCode {
-        if (isSuccess(requeteFeuille)) {
-            Feuille feuilleReponse;
-            String jsonReponse = requeteFeuille.readReponseFromAPI(requeteFeuille.getConn());
-            ObjectMapper objectMapper = new ObjectMapper();
-            feuilleReponse = objectMapper.readValue(jsonReponse, Feuille.class);
-            System.out.println(feuilleReponse.showTerrainContent());
-            return Optional.of(feuilleReponse);
+            objectResponse = objectMapper.readValue(jsonResponse, typeClass);
+            return Optional.of(objectResponse);
         }
         return Optional.empty();
     }
