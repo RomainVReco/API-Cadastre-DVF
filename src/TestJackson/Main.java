62a42dde6bcad0bd9497c5532d9039642df8d845
RomainV <romain.vorler@gmail.com>
2024-02-08 10:26:02 +0100

    ajustement commune

--- src/TestJackson/Main.java
+++ src/TestJackson/Main.java
@@ -1,30 +1,41 @@
 package TestJackson;
 
 import org.immo.exceptions.NoParcelleException;
-import org.immo.geojson.adresseban.AdresseBAN;
-import org.immo.geojson.geomutation.FeatureMutation;
 import org.immo.model.FindMutationParcelle;
+import org.immo.userinput.GestionUser;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 public class Main {
 
     public static void main(String[] args) {
-//        FindMutationFeuille fm = new FindMutationFeuille();
-//        System.out.println(fm.getSetOfGeomutations().size());
-//
+
+        boolean goingOn = true;
+        GestionUser userInput = new GestionUser();
+        String answer = "";
+
+        while (goingOn) {
+            FindMutationParcelle findMutationParcelle = null;
+            try {
+                findMutationParcelle = new FindMutationParcelle();
+            } catch (IOException | URISyntaxException | NoParcelleException e) {
+                throw new RuntimeException(e);
+            }
+
+            answer = userInput.promptYesNo("Voulez-vous continuer ? ");
+            if (answer.equals('N')) {
+                goingOn = false;
+            } else System.out.println(" ");
+        }
+
+
 
 //        System.out.println("Je lance le stream\n");
-//        Stream<FeatureMutation> fmit = fm.getSetOfGeomutations().stream();
+//        Stream<FeatureMutation> fmit = findMutationParcelle.getSetOfGeomutations().stream();
 //        List<FeatureMutation> listOfFiltered = fmit
-//                .filter(x -> x.getGeomutationPoperties().getlIdpar().contains("920400000I0045"))
-//                .filter(x -> x.getGeomutationPoperties().isVefa())
+//                .filter(x -> x.getGeomutationPoperties().getlIdpar().contains("920400000E0076"))
 //                .collect(Collectors.toList());
 //
 //        System.out.println("Fin du Stream et début de la liste de lecture \n");
@@ -32,25 +43,5 @@ public class Main {
 //        for (FeatureMutation tg : listOfFiltered){
 //            System.out.println(tg.toString());
 //        }
-
-        FindMutationParcelle findMutationParcelle = null;
-        try {
-            findMutationParcelle = new FindMutationParcelle();
-        } catch (IOException | URISyntaxException | NoParcelleException e) {
-            throw new RuntimeException(e);
-        }
-
-                System.out.println("Je lance le stream\n");
-        Stream<FeatureMutation> fmit = findMutationParcelle.getSetOfGeomutations().stream();
-        List<FeatureMutation> listOfFiltered = fmit
-                .filter(x -> x.getGeomutationPoperties().getlIdpar().contains("920400000E0076"))
-                .collect(Collectors.toList());
-
-        System.out.println("Fin du Stream et début de la liste de lecture \n");
-        System.out.println(listOfFiltered.size()+" achats VEFA parcelle 0I-45");
-        for (FeatureMutation tg : listOfFiltered){
-            System.out.println(tg.toString());
-        }
     }
-
 }
